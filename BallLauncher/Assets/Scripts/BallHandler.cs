eff468edf902ce69d89f88cc6650b58f58f712c4
Johan Andersson <johanstechs@gmail.com>
2022-12-04 01:23:24 +0100

    Launching and respawning the ball

--- BallLauncher/Assets/Scripts/BallHandler.cs
+++ BallLauncher/Assets/Scripts/BallHandler.cs
@@ -4,26 +4,73 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
   [SerializeField]
-  Rigidbody2D currentBallRigidbody;
+  GameObject ballPrefab;
+  [SerializeField]
+  Rigidbody2D pivot;
+  [SerializeField]
+  float detachDelay = 0.25f;
+  [SerializeField]
+  float respawnDelay = 0.75f;
 
+  Rigidbody2D _currentBallRigidbody;
+  SpringJoint2D _currentBallSpringJoint;
   Camera _mainCamera;
+  bool _isDragging;
 
   void Awake()
   {
     _mainCamera = Camera.main;
+    SpawnNewBall();
   }
 
   void Update()
   {
+    if (_currentBallRigidbody == null) return;
+
+    HandleBall();
+  }
+
+  void HandleBall()
+  {
     if (!Touchscreen.current.primaryTouch.press.isPressed)
     {
-      currentBallRigidbody.isKinematic = false;
+      if (_isDragging)
+      {
+        LaunchBall();
+      }
+
+      _isDragging = false;
       return;
     }
 
-    currentBallRigidbody.isKinematic = true;
+    _isDragging = true;
+    _currentBallRigidbody.isKinematic = true;
     Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
     Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
-    currentBallRigidbody.position = worldPosition;
+    _currentBallRigidbody.position = worldPosition;
+  }
+
+  void SpawnNewBall()
+  {
+    GameObject ballInstance = Instantiate(ballPrefab, pivot.position, Quaternion.identity);
+    _currentBallRigidbody = ballInstance.GetComponent<Rigidbody2D>();
+    _currentBallSpringJoint = ballInstance.GetComponent<SpringJoint2D>();
+    _currentBallSpringJoint.connectedBody = pivot;
+  }
+
+  void LaunchBall()
+  {
+    _currentBallRigidbody.isKinematic = false;
+    _currentBallRigidbody = null;
+
+    Invoke(nameof(DetachBall), detachDelay);
+  }
+
+  void DetachBall()
+  {
+    _currentBallSpringJoint.enabled = false;
+    _currentBallSpringJoint = null;
+
+    Invoke(nameof(SpawnNewBall), respawnDelay);
   }
 }
