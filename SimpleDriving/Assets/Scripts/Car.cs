b86265004695e12ab55c888b708486ac8b4b0ca3
Johan <johanstechs@gmail.com>
2022-12-09 10:47:44 +0100

    Steering and event triggers

--- SimpleDriving/Assets/Scripts/Car.cs
+++ SimpleDriving/Assets/Scripts/Car.cs
@@ -6,11 +6,25 @@ public class Car : MonoBehaviour
   float moveSpeed = 10f;
   [SerializeField]
   float accelerationSpeed = 0.5f;
+  [SerializeField]
+  float turnSpeed = 200f;
+
+  int _steerValue;
 
   void Update()
   {
-    moveSpeed += accelerationSpeed * Time.deltaTime;
+    Move();
+  }
 
+  void Move()
+  {
+    moveSpeed += accelerationSpeed * Time.deltaTime;
+    transform.Rotate(0f, _steerValue * turnSpeed * Time.deltaTime, 0f);
     transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+  }
+
+  public void Steer(int value)
+  {
+    _steerValue = value;
   }
 }
--- SimpleDriving/README.md
+++ SimpleDriving/README.md
@@ -2,8 +2,8 @@
 
 List of things or concepts utilised in this project that will be useful in the future.
 
-|  #  | Concept | Video Lesson # \* |
-| :-: | :-----: | :---------------: |
-| 01  |    ?    |         ?         |
+|  #  |    Concept     | Video Lesson # \* |
+| :-: | :------------: | :---------------: |
+| 01  | Event triggers |        23         |
 
 \* At the time of going through this course.
