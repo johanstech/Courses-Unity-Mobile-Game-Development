f0e04492d3f1202b3181bd35a8f6d728da191959
Johan <johanstechs@gmail.com>
2023-01-11 12:13:14 +0100

    Added score system

--- AsteroidAvoider/Assets/Scripts/GameOverHandler.cs
+++ AsteroidAvoider/Assets/Scripts/GameOverHandler.cs
@@ -1,16 +1,23 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverHandler : MonoBehaviour
 {
   [SerializeField]
+  TMP_Text gameOverText;
+  [SerializeField]
   GameObject gameOverDisplay;
+  [SerializeField]
+  ScoreSystem scoreSystem;
   [SerializeField]
   AsteroidSpawner asteroidSpawner;
 
   public void EndGame()
   {
     asteroidSpawner.enabled = false;
+    int finalScore = scoreSystem.StopTimer();
+    gameOverText.text = $"Score: {finalScore}";
     gameOverDisplay.gameObject.SetActive(true);
   }
 
---
+++ AsteroidAvoider/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+  [SerializeField]
+  TMP_Text scoreText;
+  [SerializeField]
+  float scoreMultiplier = 5;
+
+  float _score;
+  bool _updateScore = true;
+
+  void Update()
+  {
+    if (!_updateScore) return;
+
+    _score += Time.deltaTime * scoreMultiplier;
+    scoreText.text = Mathf.FloorToInt(_score).ToString();
+  }
+
+  public int StopTimer()
+  {
+    _updateScore = false;
+    scoreText.text = string.Empty;
+    return Mathf.FloorToInt(_score);
+  }
+
+  public void StartTimer()
+  {
+    _updateScore = true;
+    scoreText.text = Mathf.FloorToInt(_score).ToString();
+  }
+}
